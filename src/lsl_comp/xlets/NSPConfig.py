303d18a478286a61e50a030a8c9bd8a2c9dc9bc5
Akshaj <akshajverma7@gmail.com>
2025-10-15 14:22:17 +0200

    Add nsp reader from ezmsg-blackrock.

---
+++ src/lsl_comp/xlets/NSPConfig.py
@@ -0,0 +1,93 @@
+import sys
+import ezmsg.core as ez
+import typer
+from ezmsg.lsl.outlet import LSLOutletUnit
+
+from ezmsg.util.messages.key import FilterOnKey
+from typing_extensions import Annotated
+from ezmsg.blackrock.nsp import NSPSource, NSPSourceSettings
+from ezmsg.util.debuglog import DebugLog
+
+
+def main(
+    inst_addr: Annotated[
+        str,
+        typer.Option(
+            help="ipv4 address of device. pycbsdk will send control packets to this address. Subnet OK. Use 127.0.0.1 "
+            "for use with nPlayServer (non-bcast). The default is 0.0.0.0 (IPADDR_ANY) on Mac and Linux. On "
+            "Windows, known IPs will be searched."
+        ),
+    ] = "192.168.137.128",  # 192.168.137.128 local bound address as configured in most NSP settings
+    inst_port: Annotated[
+        int,
+        typer.Option(
+            help="Network port to send control packets. Use 51002 for Gemini and 51001 for Legacy NSP."
+        ),
+    ] = 51001,
+    client_addr: Annotated[
+        str,
+        typer.Option(
+            help="ipv4 address of this machine's network adapter we will receive packets on. Defaults to INADDR_ANY. "
+            "If address is provided, assumes Cerebus Subnet."
+        ),
+    ] = "",  # value -> "" for nsp, value ->"127.0.0.1" for Nplay
+    client_port: Annotated[
+        int,
+        typer.Option(
+            help="Network port to receive packets. This should always be 51002."
+        ),
+    ] = 51002,
+    recv_bufsize: Annotated[int, typer.Option(help="UDP socket recv buffer size.")] = (
+        8 if sys.platform == "win32" else 6
+    )
+    * 1024
+    * 1024,
+    protocol: Annotated[
+        str, typer.Option(help="Protocol Version. 3.11, 4.0, or 4.1 supported.")
+    ] = "4.1",
+    cont_buffer_dur: Annotated[
+        float,
+        typer.Option(
+            help="Duration of buffer for continuous data. Note: buffer may occupy ~15 MB / second."
+        ),
+    ] = 0.5,
+    microvolts: Annotated[
+        bool,
+        typer.Option(
+            help="Convert continuous data to microvolts (True) or keep raw integers (False)."
+        ),
+    ] = True,
+    cbtime: Annotated[
+        bool,
+        typer.Option(
+            help="Use Cerebus time for continuous data (True) or local time.time (False)."
+        ),
+    ] = False,  # Use local time.time if you stay on one machine, else it will introduce time mismatch in LSL
+):
+    source_settings = NSPSourceSettings(
+        inst_addr,
+        inst_port,
+        client_addr,
+        client_port,
+        recv_bufsize,
+        protocol,
+        cont_buffer_dur,
+        microvolts,
+        cbtime,
+    )
+
+    comps = {
+        "SRC": NSPSource(source_settings),
+        "LSL_OUTLET": LSLOutletUnit(stream_name="lsl_nsp", stream_type="EMG"),
+        "NS2": FilterOnKey(key="ns2"),
+        "test": DebugLog(),
+    }
+
+    conns = ((comps["SRC"].OUTPUT_SIGNAL, comps["LSL_OUTLET"].INPUT_SIGNAL),)
+
+    # NSP into LSL
+    ez.run(components=comps, connections=conns)
+
+
+if __name__ == "__main__":
+    typer.run(main)
