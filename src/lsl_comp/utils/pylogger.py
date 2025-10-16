68f6b4b3153f911ad0de6f441edd8b54b9df311e
Akshaj <akshajverma7@gmail.com>
2025-10-16 11:52:11 +0200

    Print logger path.

--- src/lsl_comp/utils/pylogger.py
+++ src/lsl_comp/utils/pylogger.py
@@ -3,7 +3,9 @@ import logging
 
 def logger_creator(verbose: bool) -> logging.Logger:
     if verbose:
-        logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+        logging.basicConfig(
+            level=logging.DEBUG
+        )  # , format="%(levelname)s: %(message)s")
     else:
         logging.basicConfig(level=logging.CRITICAL + 1)
 
