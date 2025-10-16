e0434db2487dd4072f1ecc3a6afa89e3cf747854
Akshaj <akshajverma7@gmail.com>
2025-10-16 11:54:53 +0200

    Revert pylogger to not print logger path.

--- src/lsl_comp/utils/pylogger.py
+++ src/lsl_comp/utils/pylogger.py
@@ -3,9 +3,7 @@ import logging
 
 def logger_creator(verbose: bool) -> logging.Logger:
     if verbose:
-        logging.basicConfig(
-            level=logging.DEBUG
-        )  # , format="%(levelname)s: %(message)s")
+        logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
     else:
         logging.basicConfig(level=logging.CRITICAL + 1)
 
